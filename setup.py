d244a0af76e90f746a647b49050d9ffa08f86e75
Shinya Akagi <dummy@email>
2019-03-31 20:20:49 +0900

    Update long_desctiption

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
+# -*- encoding: utf-8 -*-
+
 from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
 setup(
     name="ConvertPath",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
 
     install_requires=['pathlib'],
 
     # metadata to display on PyPI
     author="Shinya Akagi",
-    description=r"Simple Path Convertor",
-    long_description=r"Simple Path Convertor for converting windows path ('C:\\hoge') to linux path ('/C/hoge')",
+    description="Simple Path Convertor",
+    long_description=readme,
     url="https://github.com/ShinyaAkagiI/convertpath", 
     license="PSF",
 )
