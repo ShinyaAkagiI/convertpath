63215c506e3f7c56a1a5a3c2f601f927c443f1c3
Shinya Akagi <dummy@email>
2019-04-05 06:34:06 +0900

    Modified replace method to regular expression substring method, and Added feature windows2linux

--- convertpath/convertpath.py
+++ convertpath/convertpath.py
@@ -1,4 +1,5 @@
 import os
+import re
 from pathlib import Path, PurePosixPath, PureWindowsPath
 
 class convertpath:
@@ -11,15 +12,32 @@ class convertpath:
         rst = convertpath_win2linux(rawpath)
         return rst
 
+    def linux2win(self, path=None):
+        rawpath = path or self.path
+        rst = convertpath_linux2win(rawpath)
+        return rst
+
 
 def convertpath_win2linux(path):
-    r"""
-    Convert a directory path from windows path to linux path
+    """ Convert a directory path from windows path to linux path
 
-    >>> convertpath_win2linux(r"C:\Users\user")
+    >>> convertpath_win2linux(r"C:\\Users\\user")
     '/C/Users/user'
     """
-    rst = PurePosixPath(Path(path).absolute())
-    rst = str(rst).replace('C:\\', '/C').replace('c:\\', '/c')
-    return rst
 
+    rst = PurePosixPath(Path(path).absolute())
+    rst = re.sub(r'^C:\\', r'/C', str(rst))
+    rst = re.sub(r'^c:\\', r'/c', str(rst))
+    return str(rst)
+
+
+def convertpath_linux2win(path):
+    """ Convert a directory path from linux path to windows path
+
+    >>> convertpath_linux2win(r"/C/Users/user")
+    'C:\\\\Users\\\\user'
+    """
+    
+    path = re.sub(r'^/[Cc]', r'', str(path))
+    rst = PureWindowsPath(Path(path).absolute())
+    return str(rst)
