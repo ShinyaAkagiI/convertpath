8a71ea1eed3b130eee4e27888406677c9afd316a
ShinyaAkagi <ichiwri@gmail.com>
2022-11-23 17:32:14 +0900

    setup.py version up

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name="convertpath",
-    version="0.2.1",
+    version="0.2.2",
     py_modules=["convertpath"],
     package_dir = {"": "convertpath"},
     install_requires=['pathlib'],
